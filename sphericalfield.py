892958c2c9ef4b3ef48a33d474e177ebc202bd3b
markhannel <mdh386@nyu.edu>
2016-11-09 19:37:07 -0500

    Altered recursion relation (xi_nm2 and xi_nm1) to to appropriately deal with z<0. Revised checks on input.

--- sphericalfield.py
+++ sphericalfield.py
@@ -1,122 +1,47 @@
-#+
-# NAME:
-#       sphericalfield
-#
-# PURPOSE:
-#       Calculates the complex electric field defined by an array of scattering
-#       coefficients.
-#
-# CATEGORY:
-#       Holography, light scattering, microscopy
-#
-# CALLING SEQUENCE:
-#       field = sphericalfield(x, y, z, a, lambda, $
-#                           mpp = mpp)
-#
-# INPUTS:
-#       x: [npts] array of pixel coordinates [pixels]
-#       y: [npts] array of pixel coordinates [pixels]
-#       z: If field is required in a single plane, then
-#          z is the plane's distance from the sphere's center
-#          [pixels].
-#          Otherwise, z is an [npts] array of coordinates.
-#
-#       NOTE: Ideally, x, y and z should be double precision.
-#             This is left to the calling program for efficiency.
-#
-#       a: [2,nc] array of a and b scattering coefficients, where
-#          nc is the number of terms required for convergence.
-#
-#       lambda: wavelength of light in medium [pixels]
-#
-# KEYWORD FLAGS:
-#       cartesian: If set, return field components in Cartesian
-#           coordinates.  Default: Spherical polar coordinates
-#
-# OUTPUTS:
-#       field: [npts,3] complex values of field at the positions r.
-#              [:,0]: r component
-#              [:,1]: theta component
-#              [:,2]: phi component
-#
-#              If CARTESIAN is set:
-#              [:,0]: x component (incident polarization)
-#              [:,1]: y component (transverse component)
-#              [:,2]: z component (axial component, relative to
-#              incident beam).
-#
-# REFERENCE:
-#   1. Adapted from Chapter 4 in
-#      C. F. Bohren and D. R. Huffman, 
-#      Absorption and Scattering of Light by Small Particles,
-#      (New York, Wiley, 1983).
-#   2. W. J. Wiscombe,
-#      Improved Mie scattering algorithms,
-#      Applied Optics 19, 1505-1509 (1980).
-#
-# MODIFICATION HISTORY:
-# Written by David G. Grier, New York University, 5/2007
-# 6/9/2007: DGG finally read Section 4.8 in Bohren and Huffman about
-#    numerical stability of the recursions used to compute the scattering
-#    coefficients.  Feh.  Result is a total rewrite.
-# 6/20/2007: DGG Calculate \tau_n(\cos\theta) and \pi_n(\cos\theta)
-#    according to recurrence relations in 
-#    W. J. Wiscombe, Appl. Opt. 19, 1505-1509 (1980).
-#    This is supposed to improve numerical accuracy.
-# 2/8/2008: DGG. Replaced single [3,npts] array of input coordinates
-#    with two [npts] arrays for x and y, and a separate input for z.
-#    Eliminated double() call for coordinates.  Z may have 1 element or
-#    npts elements. Small documentation fixes.
-# 4/3/2008: Bo Sun (Sephiroth), NYU: Calculate Lorenz-Mie a and b
-#    coefficients using continued fractions rather than recursion.
-#    Osman Akcakir from Arryx pointed out that the results are
-#    more accurate in extreme cases.  Method described in
-#    William J. Lentz, "Generating Bessel functions in Mie scattering
-#    calculations using continued fractions," Appl. Opt. 15, 668-671
-#    (1976).
-# 4/4/2008: DGG small code clean-ups and documentation.  Added
-#    RECURSIVE keyword for backward compatibility in computing a and b
-#    coefficients.
-# 4/11/2008: Sephiroth: Corrected small error in jump code for
-#    repeated fractions in Mie coefficients.
-# 6/25/2008: DGG Don't clobber x coordinate input values.
-# 10/9/2008: DGG adapted from SPHEREFIELD by separating out
-#    calculation of scattering coefficients, a_n and b_n.  This
-#    is therefore more general, and can be replaced more
-#    readily with a GPU-accelerated version.
-# 10/13/2008: DGG eliminated RECURSIVE keyword.
-# 06/18/2010: DGG Added COMPILE_OPT.
-# 09/04/2011: DGG Compute -\tau_n rather than \tau_n
-
 import numpy as nmp
 
-def sphericalfield(x_, y_, z_, ab, lamb,cartesian = ''):
+def check_if_numpy(x, char_x):
+    ''' checks if x is a numpy array '''
+    if type(x) != nmp.array:
+        print char_x + ' must be an numpy array'
+        return True
+    else:
+        return False
+
+def sphericalfield(x, y, z, ab, lamb, cartesian = True):
     """
-    Calculates the complex electric field defined by an array of scattering
+    Calculate the complex electric field defined by an array of scattering
     coefficients.
 
-    Inputs:
-    x: [npts] array of pixel coordinates [pixels]
-    y: [npts] array of pixel coordinates [pixels]
-    z: If field is required in a single plane, then
-       z is the plane's distance from the sphere's center
-       [pixels].
-       Otherwise, z is an [npts] array of coordinates.
-    a: [2,nc] array of a and b scattering coefficients, where
-       nc is the number of terms required for convergence.
-    lambda: wavelength of light in medium [pixels]
-   
-    Example:
-    >>> field = sphericalfield(x, y, z, a, lambda, mpp = mpp)
+    Args:
+        x: [npts] array of pixel coordinates [pixels]
+        y: [npts] array of pixel coordinates [pixels]
+        z: If field is required in a single plane, then
+            z is the plane's distance from the sphere's center
+            [pixels].
+            Otherwise, z is an [npts] array of coordinates.
+        a: [2,nc] array of a and b scattering coefficients, where
+            nc is the number of terms required for convergence.
+    Keywords:
+        lamb: wavelength of light in medium [pixels]
+        cartesian: if True, field is expressed as (x,yz) else (r, theta, phi)
+    Returns:
+        field: [3,npts] scattered electricc field
     """
 
-    # (FIXME MDH): Dangerous check.
-    if type(x_) != nmp.ndarray: x_ = nmp.array(x_)
-    if type(y_) != nmp.ndarray: y_ = nmp.array(y_)
-    if type(z_) != nmp.ndarray: z_ = nmp.array(z_)
-    if type(ab) != nmp.ndarray: ab = nmp.array(ab)
+    # Check that inputs are numpy arrays
+    for var, char_var in zip([x,y,z,ab], ['x', 'y', 'z', 'ab']):
+        if check_if_numpy(var, char_var):
+            return None
 
-    npts = len(x_)
+    # Check the inputs are the right size
+    
+    if x.shape != y.shape:
+        print 'x has shape {} while y has shape {}'.format(x.shape, y.shape)
+        print 'and yet their dimensions must match.'
+        return None
+
+    npts = len(x)
     nc = len(ab[:,0])-1     # number of terms required for convergence
 
     k = 2.0 * nmp.pi / lamb # wavenumber in medium [pixel^-1]
@@ -127,14 +52,14 @@ def sphericalfield(x_, y_, z_, ab, lamb,cartesian = ''):
     # (r, theta, phi) is the spherical coordinate of the pixel
     # at (x,y) in the imaging plane at distance z from the
     # center of the sphere.
-    rho   = nmp.sqrt(x_**2 + y_**2)
-    r     = nmp.sqrt(rho**2 + z_**2)
-    theta = nmp.arctan(rho/z_)
-    phi   = nmp.arctan(y_/x_)
+    rho   = nmp.sqrt(x**2 + y**2)
+    r     = nmp.sqrt(rho**2 + z**2)
+    theta = nmp.arctan(rho/z)
+    phi   = nmp.arctan(y/x)
     costheta = nmp.cos(theta)
     sintheta = nmp.sin(theta)
-    cosphi = nmp.cos(phi)
-    sinphi = nmp.sin(phi)
+    cosphi   = nmp.cos(phi)
+    sinphi   = nmp.sin(phi)
     
     #Fix arctan results of NAN to zero
     b = nmp.where(theta ==0)[0]
@@ -148,8 +73,8 @@ def sphericalfield(x_, y_, z_, ab, lamb,cartesian = ''):
     # ... Riccati-Bessel radial functions, page 478
     sinkr = nmp.sin(kr)
     coskr = nmp.cos(kr)
-    xi_nm2 = coskr+ci*sinkr # \xi_{-1}(kr) # (FIXME MDH): + to - for z<0
-    xi_nm1 = sinkr-ci*coskr # \xi_0(kr)    # (FIXME MDH): - to + for z<0
+    xi_nm2 = coskr + nmp.sign(z)*ci*sinkr # \xi_{-1}(kr) 
+    xi_nm1 = sinkr - nmp.sign(z)*ci*coskr # \xi_0(kr)    
 
     # ... angular functions (4.47), page 95
     pi_nm1 = 0.0                    # \pi_0(\cos\theta)
