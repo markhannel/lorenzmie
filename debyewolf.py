75a1ef314f2e676303307306126e5b8ff6fa051d
markhannel <mdh386@nyu.edu>
2016-12-05 15:25:55 -0500

    debyewolf produces an image, albeit not completely correct.

--- debyewolf.py
+++ debyewolf.py
@@ -1,6 +1,7 @@
 import numpy as nmp
 from lorenzmie import lm_angular_spectrum
 from sphere_coefficients import sphere_coefficients
+import matplotlib.pyplot as plt
 
 def phase_displace(x, y, z, r, k):
     ''' Determines the phase due to displacement. '''
@@ -19,13 +20,22 @@ def check_if_numpy(x, char_x):
     else:
         return True
 
+def aperture(field, x, y, r_max):
+    '''Sets field to zero wherever x**2+y**2 >= rmax.'''
+    r_2 = x**2+y**2
+    indices = nmp.where(r_2 >= r_max**2)
+
+    field[:,indices] = 0
+    return field
+
 def discretize_plan(NA, M, lamb, nm_img, mpp):
     '''
     Discretizes a plane according to Eq. 130 - 131.
     '''
     # Suppose the largest scatterer we consider is 20 lambda. THen
     # P should be larger than 40*NA.
-    p, q = int(40*NA), int(40*NA)
+    diam = 200 # wavelengths
+    p, q = int(diam*NA), int(diam*NA)
 
     # Pad with zeros for increased resolution and to set del_x to mpp.
     pad_p = int((lamb - mpp*2*NA)/(mpp*2*NA)*p)
@@ -40,15 +50,17 @@ def discretize_plan(NA, M, lamb, nm_img, mpp):
     return pad_p, pad_q, p, q, sx_img, sy_img
     
 
-def consv_energy(es, sx_obj, sy_obj, sx_img, sy_img):
+def consv_energy(es, sx_obj, sy_obj, sx_img, sy_img, r_max):
     '''
     Changes electric field strength factor density to obey the conversation
     of energy.
     '''
+    r_2 = sx_obj**2 +sy_obj**2
+    indices = nmp.where(r_2 <= r_max**2)
     # Compute the necessary cosine terms in Eq 108.
     cos_theta_img = nmp.sqrt(1. - (sx_img**2+sy_img**2))
     cos_theta_obj = nmp.sqrt(1. - (sx_obj**2+sy_obj**2))
-    es *= nmp.sqrt(cos_theta_img/cos_theta_obj)
+    es[:,indices[0],indices[1]] *= nmp.sqrt(cos_theta_img[indices[0],indices[1]]/cos_theta_obj[indices[0],indices[1]])
 
     return es
 
@@ -87,6 +99,8 @@ def debyewolf(z, ap, np, lamb = 0.447, mpp = 0.135, dim = [201,201], NA = 1.45,
 
     # Compute grids sx_obj and sx_img.
     pad_p, pad_q, p, q, sx_img, sy_img = discretize_plan(NA, M, lamb, nm_img, mpp)
+    pad_p = 0
+    pad_q = 0 
     Np = pad_p + p
     Nq = pad_q + q
 
@@ -94,77 +108,113 @@ def debyewolf(z, ap, np, lamb = 0.447, mpp = 0.135, dim = [201,201], NA = 1.45,
 
     sx_obj = M*nm_img/nm_obj*sx_img
     sy_obj = M*nm_img/nm_obj*sy_img
-    
-    print max(sx_img**2+sy_img**2)
-    print max(sx_obj**2+sy_obj**2)
 
+    # Compute the angular spectrum incident on plane 1.
     # Compute the electromagnetic strength factor on the object side (Eq 40 Ref[1]).
     ab = sphere_coefficients(ap,np,lamb,mpp)
     es_obj = lm_angular_spectrum(sx_obj, sy_obj, ab, lamb, nm_obj, f/M)
     es_obj = nmp.nan_to_num(es_obj)
+
+    temp = es_obj.reshape(3,p,q)
+    plt.imshow(nmp.hstack([nmp.abs(temp[0]), nmp.abs(temp[1]), nmp.abs(temp[2])]))
+    plt.title('Electric Field strength (Theta comp) at P_1')
+    plt.show()
+
+
+    # Apply the aperture function.
+    es_obj = aperture(es_obj, sx_obj, sy_obj, NA/nm_obj)
     es_obj = es_obj.reshape(3,p,q)
-    
-    import matplotlib.pyplot as plt
-    plt.imshow(nmp.real(es_obj[0,:,:]))
+
+
+    plt.imshow(nmp.hstack([nmp.abs(es_obj[0]), nmp.abs(es_obj[1]), nmp.abs(es_obj[2])]))
+    plt.title('Electric Field strength (Theta comp) at P_1 after aperture')
     plt.show()
 
     sx_img = nmp.arange(p, dtype = float)
     sy_img = nmp.arange(q, dtype = float)
     
-    sx_img = NA/(M*nm_img)*( (1+2*sx_img)/(p-1) - 1)
-    sy_img = NA/(M*nm_img)*( (1+2*sy_img)/(q-1) - 1)
+    sx_img = NA/(M*nm_img)*( (1+2*sx_img)/p - 1)
+    sy_img = NA/(M*nm_img)*( (1+2*sy_img)/q - 1)
     
-    sx_obj = M*nm_img/nm_obj*sx_img
-    sy_obj = M*nm_img/nm_obj*sy_img
-
     # Displace the field.
     #es *= phase_displace(x, y, z, r, k)
 
+    sxx_img, syy_img = nmp.meshgrid(sx_img, sy_img)
+    sintheta = sxx_img**2 + syy_img**2
+    costheta = nmp.sqrt(1. - sintheta)
+    sintheta = nmp.sqrt(sintheta)
+
+    sxx_obj = M*nm_img/nm_obj*sxx_img
+    syy_obj = M*nm_img/nm_obj*syy_img
+
+    # Compute the electric field strength factor on plane 2.
     # Ensure conservation of energy is observed with abbe sine condition.
-    es_img = consv_energy(es_obj, sx_obj, sy_obj, sx_img, sy_img)
+    es_img = consv_energy(es_obj, sxx_obj, syy_obj, sxx_img, syy_img, NA/nm_obj)
     es_img = remove_r(es_img) # Should be no r component.
 
-    # Compute auxiliary (Eq. 133) with zero padding!
-    # (FIXME MDH: Set up a 2d version of sx_img, sy_img with meshgrid)
-    #aber  = nmp.zeros([3, Np, Nq], complex) # As a function of sx_img, sy_img
-    sxx_img, syy_img = nmp.meshgrid(sx_img, sy_img)
-    sintheta = sxx_img**2 + syy_img**2
-    costheta = nmp.sqrt(1 - sintheta)
-    sintheta = nmp.sqrt(sintheta)
+    es_img = nmp.nan_to_num(es_img)
     
-    g_aux = nmp.zeros([3, p, q], complex)
+
+    plt.imshow(nmp.hstack([nmp.abs(es_img[0]), nmp.abs(es_img[1]), nmp.abs(es_img[2])]))
+    plt.title('Electric Field strength (Theta comp) at P_2')
+    plt.show()
+
+
+    # Compute auxiliary (Eq. 133) with zero padding!
+    #aber  = nmp.zeros([3, Np, Nq], complex) # As a function of sx_img, sy_img
+    g_aux = nmp.zeros([3, Np, Nq], complex)
     for i in xrange(3):
-        g_aux[i,:,:] = es_img[i,:,:]/costheta
+        #g_aux[i, pad_p/2:-pad_p/2, pad_q/2:-pad_q/2] = es_img[i,:,:]/costheta
+        g_aux[i, :,:] = es_img[i,:,:]/costheta
         #g_aux *= nmp.exp(-1.j*k_img*aber)
 
-    # Apply discrete Fourier Transform (Eq. 135).
-    es_m_n = nmp.fft.fft2(g_aux, s = (Np, Nq))
-    es_m_n = nmp.fft.fftshift(es_m_n)
+    plt.imshow(nmp.hstack([nmp.abs(g_aux[0]), nmp.abs(g_aux[1]), nmp.abs(g_aux[2])]))
+    plt.title('Auxiliary field (Theta comp) at P_2')
+    plt.show()
 
-    # Compute the electric field at the image while accounting for aliasing.
-    es_cam  = (1.j*NA**2/(M**2*nm_img*lamb))*(4/npts)*es_m_n
-    m = nmp.arange(0,Np)
-    n = nmp.arange(0,Nq)
+
+    # Apply discrete Fourier Transform (Eq. 135).
+    es_m_n = nmp.fft.fft2(g_aux, s = (Np,Nq))
+    for i in xrange(3):
+        es_m_n[i] = nmp.fft.fftshift(es_m_n[i])
+
+    plt.imshow(nmp.hstack([nmp.abs(es_m_n[0]), nmp.abs(es_m_n[1]), nmp.abs(es_m_n[2])]))
+    plt.title('Fourier Transform of aux')
+    plt.show()
+
+
+    # Compute the electric field at plane 3.
+    # Accounting for aliasing.
+    es_cam  = (1.j*NA**2/(M*lamb))*(4./npts)*es_m_n
+
+    m = nmp.arange(0,Np, dtype = float)
+    n = nmp.arange(0,Nq, dtype = float)
     mm, nn = nmp.meshgrid(m,n)
     for i in xrange(3):
-        es_cam[i,:,:] *= nmp.exp(-2*nmp.pi*1.j*( (1-p)/(p**2*Np*mm + (1-q)/(q**2*Nq)*nn)))
+        es_cam[i,:,:] *= nmp.exp(-1.j*nmp.pi*( mm*(1.-p)/Np + nn*(1.-q)/Nq))
 
-    sx_img = nmp.arange(Np, dtype = float)
-    sy_img = nmp.arange(Nq, dtype = float)
-    
-    sx_img = NA/(M*nm_img)*( (1+2*sx_img)/(p-1) - 1)
-    sy_img = NA/(M*nm_img)*( (1+2*sy_img)/(q-1) - 1)
+    plt.imshow(nmp.hstack([nmp.abs(es_cam[0]), nmp.abs(es_cam[1]), nmp.abs(es_cam[2])]))
+    plt.title('Electric field (theta) at the camera plane after dealiasing')
+    plt.show()
 
-    sxx_img, syy_img = nmp.meshgrid(sx_img, sy_img)
-    sintheta = sxx_img**2 + syy_img**2
+    nx_img = nmp.arange(Np, dtype = float)
+    ny_img = nmp.arange(Nq, dtype = float)
+
+    nx_img = NA/(M*nm_img)*((1+2*nx_img-pad_p)/p-1)
+    ny_img = NA/(M*nm_img)*((1+2*ny_img-pad_q)/q-1)
+
+    nxx_img, nyy_img = nmp.meshgrid(nx_img, ny_img)
+    sintheta = nxx_img**2 + nyy_img**2
+    print 'hmm...'
     costheta = nmp.sqrt(1 - sintheta**2)
+    sintheta = nmp.sqrt(sintheta)
 
-    cosphi = sxx_img/sintheta
-    sinphi = syy_img/sintheta
+    cosphi = nxx_img/sintheta
+    sinphi = nyy_img/sintheta
 
     # Convert E_img to cartesian coords
-    es_cam_cart = nmp.zeros([3, Np, Nq], complex)
-    es_cam_cart += es_cam 
+    es_cam_cart = nmp.zeros([3,Np,Nq], dtype = complex)
+    es_cam_cart += es_cam
 
     es_cam_cart[0,:,:] =  es_cam[0,:,:] * sintheta * cosphi
     es_cam_cart[0,:,:] += es_cam[1,:,:] * costheta * cosphi
@@ -175,13 +225,26 @@ def debyewolf(z, ap, np, lamb = 0.447, mpp = 0.135, dim = [201,201], NA = 1.45,
     es_cam_cart[1,:,:] += es_cam[2,:,:] * cosphi
 
     es_cam_cart[2,:,:] =  es_cam[0,:,:] * costheta - es_cam[1,:,:] * sintheta
+
     
+    plt.imshow(nmp.hstack([nmp.abs(es_cam_cart[0]), nmp.abs(es_cam_cart[1]), nmp.abs(es_cam_cart[2])]))
+    plt.title('Electric field (cart) at the camera plane after dealiasing')
+    plt.show()
+
     # Recombine with plane wave.
-    path_len = f + f/M
+    #path_len = f + f/M
+    path_len = 0.2/k_img
     es_cam_cart[0,:,:] += 1.0*nmp.exp(-1.j*k_img*path_len) # Plane wave normalized to amplitude 1.
     
-    image = nmp.sum(nmp.real(es_cam_cart*nmp.conj(es_cam_cart)), axis = 0)
+    image = nmp.sum(nmp.real(es_cam_cart*nmp.conjugate(es_cam_cart)), axis = 0)
+    real_part = nmp.sum(nmp.real(es_cam_cart), axis = 0)
+    imag_part = nmp.sum(nmp.imag(es_cam_cart), axis = 0)
 
+    print nmp.mean(image)
+    plt.imshow(nmp.hstack([real_part,imag_part]))
+    plt.title('Es_cam_cart real and imaginary')
+    plt.show()
+    
     return image
 
 def test_discretize():
@@ -203,9 +266,9 @@ def test_debye():
     image = debyewolf(z, ap, np, lamb = 0.447, mpp = 0.135, dim = [201,201], NA = 1.45, 
                       nm_obj = 1.339, nm_img = 1.0, M = 100, f = 20.*10**5)
 
-    print image[0:10]
     import matplotlib.pyplot as plt
     plt.imshow(image)
+    plt.title('Image')
     plt.gray()
     plt.show()
 
