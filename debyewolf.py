e49f88f8810a1975e4557ef65dff784f9c760400
davidruffner <davidruffner@gmail.com>
2017-01-17 23:05:37 -0500

    Calculate angular spectrum from electric strength factor.

--- debyewolf.py
+++ debyewolf.py
@@ -1,7 +1,7 @@
 ## FIXME: Determine if you should use fftshift or ifftshift or a 2d shift.
 
 import numpy as np
-from lorenzmie import lm_angular_spectrum
+from lorenzmie import lm_electric_strength_factor, lm_angular_spectrum
 from sphere_coefficients import sphere_coefficients
 import matplotlib.pyplot as plt
 import geometry as g
@@ -67,11 +67,11 @@ def remove_r(es):
     
 def scatter(s_obj_cart, a_p, n_p, nm_obj, lamb, r, z):
     '''Compute the angular spectrum arriving at the entrance pupil.'''
-    
-    # Compute the electromagnetic strength factor on the object side 
+
+    # Compute the electromagnetic strength factor on the object side
     # (Eq 40 Ref[1]).
     ab = sphere_coefficients(a_p, n_p, nm_obj, lamb)
-    es_obj = lm_angular_spectrum(s_obj_cart, ab, lamb, nm_obj, r, z)
+    es_obj = lm_electric_strength_factor(s_obj_cart, ab, lamb, nm_obj, r, z)
     es_obj = np.nan_to_num(es_obj)
 
     # Apply the aperture function. (FIXME (MDH): Check isn't necessary)
@@ -79,6 +79,20 @@ def scatter(s_obj_cart, a_p, n_p, nm_obj, lamb, r, z):
 
     p, q = s_obj_cart.shape
     return es_obj.reshape(3,p,q)
+
+def scatter_ang_spectrum(s_obj_cart, a_p, n_p, nm_obj, lamb):
+    '''Compute the angular spectrum arriving at the entrance pupil.'''
+
+    # Compute the electromagnetic strength factor on the object side
+    # (Eq 40 Ref[1]).
+    ab = sphere_coefficients(a_p, n_p, nm_obj, lamb)
+    ang_spec_obj = lm_angular_spectrum(s_obj_cart, ab, lamb, nm_obj)
+    ang_spec_obj = np.nan_to_num(ang_spec_obj)
+
+    # Apply the aperture function. (FIXME (MDH): Check isn't necessary)
+    #es_obj = aperture(es_obj, s_obj_cart, NA/nm_obj)
+
+    return ang_spec_obj
 
 def collection(es_obj, s_obj_cart, s_img_cart, nm_obj, NA):
     '''Compute the angular spectrum leaving the exit pupil.'''
@@ -206,6 +220,71 @@ def debyewolf(z, a_p, n_p, lamb = 0.447, mpp = 0.135, dim = [201,201], NA = 1.45
 
     return image
 
+
+def focalPlaneField(z, a_p, n_p, lamb=0.447, mpp=0.135, dim=[201, 201], NA=1.45,
+              nm_obj=1.339, nm_img=1.0, M=100, f=20. * 10 ** 5, quiet=True):
+    '''
+    Colculate field at focal plane with angular spectrum.
+
+    Args:
+        x,y:   [pix] define where the image will be evaluated.
+        z:     [um] scatterer's distance from the focal plane.
+        a_p:   [um] sets the radius of the spherical scatterer.
+        n_p:   [R.I.U.] sets the refractive index of the scatterer.
+        nm:    [R.I.U.] sets the refractive index of the medium.
+        lamb:  [um] sets the wavelength of the coherent illumination.
+               Default: 0.447 um.
+        mpp:   [um/pix] sets the size of a pixel.
+               Default: 0.135.
+
+    Return:
+
+    Ref[1]: Capoglu et al. (2012). "The Microscope in a Computer:...",
+            Applied Optics, 38(34), 7085.
+    '''
+
+    # Necessary constants.
+    k_img = 2 * np.pi * nm_img / lamb
+
+    # Devise a discretization plan.
+    pad_p, pad_q, p, q = discretize_plan(NA, M, lamb, nm_img, mpp)
+    Np = pad_p + p
+    Nq = pad_q + q
+
+    # Compute the three geometries, s_img, s_obj, n_img
+    # Origins for the coordinate systems.
+    origin = [.5 * (p - 1.), .5 * (q - 1.)]
+
+    # Scale factors.
+    img_factor = 2 * NA / (M * nm_img)
+    obj_factor = 2 * NA / nm_obj
+    img_scale = [img_factor * 1. / p, img_factor * 1. / q]
+    obj_scale = [obj_factor * 1. / p, obj_factor * 1. / q]
+
+    # Cartesian Geometries.
+    # FIXME (MDH): is it necessary to have s_obj_cart?
+    s_img_cart = g.CartesianCoordinates(p, q, origin, img_scale)
+    s_obj_cart = g.CartesianCoordinates(p, q, origin, obj_scale)
+    n_disc_grid = g.CartesianCoordinates(Np, Nq)
+    n_img_cart = g.CartesianCoordinates(Np, Nq, [.5 * (Np - 1.), .5 * (Nq - 1.)], img_scale)
+
+    # Spherical Geometries.
+    sph_img = g.SphericalCoordinates(s_img_cart)
+    sph_n_img = g.SphericalCoordinates(n_img_cart)
+
+    # 0) Propagate the Incident field to the camera plane.
+    # FIXME: Implement separately from image_formation.
+
+    # 1) Scattering.
+    # Compute the angular spectrum incident on plane 1.
+    es_obj = scatter_ang_spectrum(s_obj_cart, a_p, n_p, nm_obj, lamb)
+
+    # TODO: Propagate in z, then convert from angular spectrum to electric field.
+
+    print es_obj
+
+    return es_obj
+
 def test_discretize():
     NA = 1.45
     M = 100
@@ -216,6 +295,26 @@ def test_discretize():
     
     del_x = lamb*p*M/(2*NA*(pad_p+p))
     print del_x/M
+
+def test_angularSpectrum():
+    z = 10.
+    a_p = 1.0
+    n_p = 1.4
+    image = focalPlaneField(z, a_p, n_p, lamb = 0.447, mpp = 0.135, dim = [201,201], NA = 1.45,
+              nm_obj = 1.339, nm_img = 1.0, M = 100, f = 20.*10**5, quiet = False)
+    print image
+    print image.shape
+    imageDHM = spheredhm([0, 0 , z], a_p, n_p, 1.339, [201, 201], mpp=0.135, lamb=0.447)
+    import matplotlib.pyplot as plt
+    plt.imshow(np.abs(np.hstack([image[0], image[1], image[2]])))
+    plt.gray()
+    plt.savefig('angularFocalPlane_ps_1.0.png')
+    plt.show()
+
+    plt.imshow(imageDHM)
+    plt.gray()
+    plt.savefig('sphereDHM_ps_1.0.png')
+    plt.show()
 
 def test_debye():
     z = 10.
@@ -283,4 +382,5 @@ def test_plots(es_obj, es_img, es_m_n, es_cam_cart):
 
 
 if __name__ == '__main__':
-    test_debye()
+    #test_debye()
+    test_angularSpectrum()
