David Ruffner correctly pointed out that the propagation direction in propagate_plane_wave and the displacement used in displace were in the wrong direction. Necessary negative signs added.

debyewolf.py
@@ -16,7 +16,7 @@ def check_if_numpy(x, char_x):
     else:
         return True
 
-def verbose(data, title, gray = False):
+def verbose(data, title, gray=False):
     plt.imshow(data)
     plt.title(title)
     if gray:
@@ -45,7 +45,7 @@ def displacement(s_obj_cart, z, k):
 
     inside = sxx**2+syy**2 < 1.
     disp = np.zeros(sxx.shape, dtype = complex)
-    disp[inside] = np.exp(1.0j * k * z * np.sqrt( 1. - sxx[inside]**2 - syy[inside]**2))
+    disp[inside] = np.exp(-1.0j * k * z * np.sqrt( 1. - sxx[inside]**2 - syy[inside]**2))
     return disp
 
 def discretize_plan(NA, M, lamb, nm_img, mpp):
@@ -78,7 +78,7 @@ def propagate_plane_wave(amplitude, k, path_len, shape):
     The wave is polarized in the x direction. The field is given as a 
     cartesian vector field.'''
     e_inc = np.zeros(shape, dtype = complex)
-    e_inc[0, :, :] += amplitude*np.exp(-1.j * k * path_len)
+    e_inc[0, :, :] += amplitude*np.exp(1.j * k * path_len)
     return e_inc
 
 def scatter(s_obj_cart, a_p, n_p, nm_obj, lamb, r, mpp):
@@ -98,9 +98,9 @@ def scatter(s_obj_cart, a_p, n_p, nm_obj, lamb, r, mpp):
     # Compute the electromagnetic strength factor on the object side 
     # (Eq 40 Ref[1]).
     ang_spec = np.zeros((3,p*q), dtype = complex)
-    ang_spec[:,inds] = sphericalfield(sx[inds]*r, sy[inds]*r, costheta[inds]*r, 
-                                      ab, lamb_m, cartesian=False, 
-                                      str_factor=True)
+    ang_spec[:, inds] = sphericalfield(sx[inds]*r, sy[inds]*r, costheta[inds]*r, 
+                                       ab, lamb_m, cartesian=False, 
+                                       str_factor=True)
 
     return ang_spec.reshape(3, p, q)
 