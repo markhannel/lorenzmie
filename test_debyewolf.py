6549de64a01565eb3d1e734985be96b929611002
markhannel <mdh386@nyu.edu>
2017-04-03 14:51:47 -0400

    Reduced the computation in test_propagate_ang_spec.

--- test_debyewolf.py
+++ test_debyewolf.py
@@ -4,6 +4,7 @@ import numpy.testing as npt
 import geometry as g
 import matplotlib.pyplot as plt
 import spheredhm as sph
+import azimedian as azi
 
 def test_propagate_plane_wave():
     shape = (3,1,1)
@@ -64,60 +65,20 @@ def test_propagate_ang_spec_microscope():
     '''
     # Necessary physical parameters.
     z = 200. # [pix]
-    a_p = 0.5 # [um]
-    n_p = 1.5 # [arb] 
+    a_p = 1.5 # [um]
+    n_p = 1.65 # [arb] 
     nm_obj = 1.339 # [arb]
     nm_img = 1.339 # [arb]
     lamb = 0.447 # [um]
-    mpp = 0.135 # [um/pix]
-    NA = 1.45
+    mpp = 0.1350981 # [um/pix]
+    NA = 1.339
     M = 1. # [arb]
 
-    k_obj = 2 * np.pi * nm_obj / lamb * mpp  # [pix**-1]
-    r_max = 1000.  # [pix]
-    sintheta_img = NA / (M * nm_img)
-
-    # Devise a discretization plan.
-    pad_p, pad_q, p, q = dw.discretize_plan(NA, M, lamb, nm_img, mpp)
-    Np = pad_p + p
-    Nq = pad_q + q
-
-    # Compute the three geometries, s_img, s_obj, n_img
-    # Origins for the coordinate systems.
-    origin = [.5*(p-1.), .5*(q-1.)]
+    es_cam = dw.particle_field_camera_plane(z, a_p, n_p, nm_obj=nm_obj, 
+                                            nm_img=nm_img, NA=NA,
+                                            lamb=lamb, mpp=mpp, M=M,
+                                            quiet=True)
     
-    # Scale factors for the coordinate systems.
-    img_factor = 2*NA/(M*nm_img)
-    obj_factor = 2*NA/nm_obj
-    img_scale = [img_factor*1./p, img_factor*1./q]
-    obj_scale = [obj_factor*1./p, obj_factor*1./q]
-
-    # Cartesian Geometries.
-    # FIXME (MDH): is it necessary to have s_obj_cart?
-    s_obj_cart = g.CartesianCoordinates(p, q, origin, obj_scale)
-    s_img_cart = g.CartesianCoordinates(p, q, origin, img_scale)
-    n_disc_grid = g.CartesianCoordinates(Np, Nq, origin=[.5*(Np-1), .5*(Nq-1.)])
-    n_img_cart  = g.CartesianCoordinates(Np, Nq, [.5*(Np-1.), .5*(Nq-1.)], 
-                                         img_scale)
-
-    # Spherical Geometries.
-    s_obj_cart.acquire_spherical(1.)
-    s_img_cart.acquire_spherical(1.)
-    n_img_cart.acquire_spherical(1.)
-
-    # Compute the angular spectrum incident on entrance pupil of the objective.
-    ang_spec = dw.scatter(s_obj_cart, a_p, n_p, nm_obj, lamb, r_max, mpp)
-
-    # Propagate the angular spectrum a distance z_p.
-    disp = dw.displacement(s_obj_cart, z, k_obj)
-    ang_spec[1:, :] *= disp
-
-    # Collection and refocus.
-    es_cam = dw.propagate_ang_spec_microscope(ang_spec, s_obj_cart, s_obj_cart, 
-                                              nm_obj, nm_img, M, n_disc_grid,
-                                              p, q, Np, Nq, NA, lamb, mpp, 
-                                              quiet=True)
-
     # Image formation.
     e_inc = dw.incident_field_camera_plane(nm_obj, nm_img, lamb, mpp, NA, M, z)
 
@@ -125,16 +86,33 @@ def test_propagate_ang_spec_microscope():
 
     # Produce image in focal plane with spheredhm.
     dim = cam_image.shape
-    focal_image = sph.spheredhm([0, 0, z], a_p, n_p, nm_obj, dim, mpp, lamb)
+    focal_image = sph.spheredhm([-0.5, -0.5, z], a_p, n_p, nm_obj, dim, mpp, lamb)
     
-    print np.max(focal_image)
-    print np.max(cam_image)
+    # PYTEST
+    #npt.assert_array_almost_equal(focal_image, cam_image, decimal = 2)
+    
+    # Temporary debugging information.
+    # Numerical information.
+    print('Mean and max value of camera image: {} {}'.format(np.mean(cam_image), np.max(cam_image)))
 
-    del_x = lamb*p*M/(2*NA*(pad_p+p))
-    print del_x
+    print('Mean and max value of focal plane image: {} {}'.format(np.mean(focal_image), np.max(focal_image)))   
+    
 
-    plt.imshow(cam_image-focal_image)
-    plt.gray()
+    # 2D images
+    dw.verbose(np.hstack([cam_image, focal_image, 1 + cam_image - focal_image]), 'Camera Image, Focal Image, Difference', gray = True)
+
+    
+    xc, yc = (dim[0]-1.)/2, (dim[1]-1)/2.
+    cam_rad = azi.azimedian(cam_image)
+    focal_rad = azi.azimedian(focal_image) 
+    # FIXME (MDH): center is not correct...
+
+    end = 150
+    plt.plot(cam_rad[:end], 'r', label = 'Camera Plane')
+    plt.plot(focal_rad[:end], 'black', label = 'Focal Plane')
+    plt.xlabel('Radial distance [pix]')
+    plt.ylabel('Normalized Intensity [arb]')
+    plt.legend()
     plt.show()
 
 if __name__ == '__main__':
